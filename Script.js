fc2964f5ad643b538d7172575305ed4421091f00
Victoran0 <victoranosunkvic@gmail.com>
2023-05-18 21:59:33 +0100

    finding phone number section

--- Script.js
+++ Script.js
@@ -6,34 +6,50 @@ const detailsBtn = () => {
 
 // let nIntervId;
 
-const getContactInfo = () => {
-    const nameEle = document.getElementById('pv-contact-info');
-    const profile = document.getElementsByClassName('pv-contact-info__contact-link link-without-visited-state')[0];
-    const websiteEle = document.getElementsByClassName('pv-contact-info__contact-link link-without-visited-state')[1];
-    const emailEle = document.getElementsByClassName('pv-contact-info__contact-link link-without-visited-state')[2];
-    const phoneEle = document.querySelector('.t-14.t-black.t-normal');
 
-    const name = nameEle.textContent.split('\n')[1].trimStart();
-    const profileLink = profile.href
-    // const email = emailEle.href.split(':')[1]
-    // const phoneNum = phoneEle.textContent.split('\n')[1].trimStart()
-    
+
+const getContactInfo = () => {
+    const name = document.getElementById('pv-contact-info').textContent.split('\n')[1].trimStart();
     console.log('name:', name)
+    
+    const profileLink = document.getElementsByClassName('pv-contact-info__contact-link link-without-visited-state')[0].href
     console.log('profileLink:', profileLink)
-    if (websiteEle){
-        const website = websiteEle.href
-        console.log('website:', website)
+
+
+    const addressEle = document.querySelector('.pv-contact-info__contact-type.ci-address');
+    if (addressEle){
+        const address = addressEle.querySelector('.pv-contact-info__contact-link.link-without-visited-state.t-14').textContent.split('\n')[1].trimStart()
+        console.log('address:', address)
+    } 
+
+    const websitesEle = document.querySelector('.pv-contact-info__contact-type.ci-websites');
+    if (websitesEle){
+        const websites = websitesEle.getElementsByClassName('pv-contact-info__contact-link link-without-visited-state')
+        for (let i=0; i<websites.length; i++) {
+            console.log('website:', websites[i].href) 
+        }      
     }
-    // if (website)console.log('website:', website)
-    // if (email) console.log('email:', email)
-    // if (phoneNum)console.log('phoneNum:', phoneNum)
+    
+    const emailEle = document.querySelector('.pv-contact-info__contact-type.ci-email')
+    if (emailEle) {
+        const email = emailEle.querySelector('.pv-contact-info__contact-link.link-without-visited-state.t-14').href.split(':')[1]
+        console.log('email:', email)
+    } 
+    
+    // Get the section instead so it does not jam
+    const phoneEle = document.querySelector('.t-14.t-black.t-normal');
+    if (phoneEle){
+        const phoneNum = phoneEle.textContent.split('\n')[1].trimStart()
+        console.log('phoneNum:', phoneNum)
+    } 
+    
+    
 }
 
 
 
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
     if (obj.message === 'profile page') {
-        console.log(obj.message)
         if (detailsBtn) {
             detailsBtn().click()
             // clearInterval(nIntervId)
@@ -46,9 +62,7 @@ chrome.runtime.onMessage.addListener((obj, sender, response) => {
     }
 
     if (obj.message === 'scrape data') {
-        console.log(obj.message + ' oya oo')
         getContactInfo()
     }
 })
-console.log('loaded')
 
